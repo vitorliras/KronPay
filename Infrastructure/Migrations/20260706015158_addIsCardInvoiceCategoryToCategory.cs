021ac81f32f0f67137146fca9950244a03d66c3b
vitorliras <vitorlirasti@hotmail.com>
2026-07-05 22:52:27 -0300

    feat: add IsCardInvoiceCategory field to Category

---
+++ Infrastructure/Migrations/20260706015158_addIsCardInvoiceCategoryToCategory.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class addIsCardInvoiceCategoryToCategory : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "is_card_invoice_category",
+                table: "category",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "is_card_invoice_category",
+                table: "category");
+        }
+    }
+}
