e37dea7c66acc7ce59962a01b9989990f27a1dea
vitorliras <vitorlirasti@hotmail.com>
2026-07-11 16:42:03 -0300

    feat: gera migration para tabela data_retention_purge_runs

---
+++ Infrastructure/Migrations/20260711194120_add-data-retention-purge-run.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class adddataretentionpurgerun : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "data_retention_purge_runs",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    last_run_at = table.Column<DateTime>(type: "datetime2(0)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_data_retention_purge_runs", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "data_retention_purge_runs");
+        }
+    }
+}
